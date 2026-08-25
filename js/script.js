388ed7fa826fd389320f340bacd3f66baaa0e173
Posa Karthik <posakarthik16@gmail.com>
2026-08-25 23:55:11 +0530

    feat: added toggle interaction for welcome message

--- js/script.js
+++ js/script.js
@@ -4,4 +4,13 @@ DevLaunch JavaScript
 ========================
 */
 
-console.log("DevLaunch Loaded Successfully🚀");
+console.log("DevLaunch JavaScript Started 🚀");
+
+const getStartedBtn = document.getElementById("getStartedBtn");
+
+const startMessage = document.getElementById("startMessage");
+
+getStartedBtn.addEventListener("click", function (event){
+    event.preventDefault();
+    startMessage.classList.toggle("d-none");
+})
