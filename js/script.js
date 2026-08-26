0116ef0d3bfb19b22cccb8cfedbca421a5f559ab
Posa Karthik <posakarthik16@gmail.com>
2026-08-26 23:27:25 +0530

    feat: make an statitics to dynamic

--- js/script.js
+++ js/script.js
@@ -14,3 +14,24 @@ getStartedBtn.addEventListener("click", function (event){
     event.preventDefault();
     startMessage.classList.toggle("d-none");
 })
+
+const streakCount = document.getElementById("streakCount");
+
+const topicsCount = document.getElementById("topicsCount");
+
+const problemsCount = document.getElementById("problemsCount");
+
+const projectsCount = document.getElementById("projectsCount");
+
+const streak = 30;
+const topics = 33;
+const problems = 100;
+const projects = 10;
+
+streakCount.textContent = streak;
+
+topicsCount.textContent = topics;
+
+problemsCount.textContent = problems;
+
+projectsCount.textContent = projects;
